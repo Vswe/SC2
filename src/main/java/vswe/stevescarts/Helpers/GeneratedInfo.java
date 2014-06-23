281a3052f4bc94cab61074bb0984892fb0831b8b
Vswe <vidsw926@student.liu.se>
2014-06-23 18:38:51 +0200

    Fixed small localization syntax issue.

--- src/main/java/vswe/stevescarts/Helpers/GeneratedInfo.java
+++ src/main/java/vswe/stevescarts/Helpers/GeneratedInfo.java
@@ -1,6 +1,6 @@
 package vswe.stevescarts.Helpers;
 
 public class GeneratedInfo {
-	public static final String version = /*@v*/"2.0.0.b11";
-	public static final boolean inDev = /*@d*/true;
+	public static final String version = /*@v*/"2.0.0.b12";
+	public static final boolean inDev = /*@d*/false;
 }
